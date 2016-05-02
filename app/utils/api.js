cff374a88f7b5bffe64a7a0934d2434a76004eef
Juan Bernardo <jrivera@hugeinc.com>
2016-05-02 17:18:06 -0500

    Notes component

--- app/utils/api.js
+++ app/utils/api.js
@@ -9,3 +9,18 @@ export function getRepos(userName) {
   const url = `https://api.github.com/users/${userName}/repos`;
   return fetch(url).then((res) => res.json());
 }
+
+export function getNotes(username) {
+  username = username.toLowerCase().trim();
+  const url = `https://jbgithub-notetaker.firebaseio.com/${username}.json`;
+  return fetch(url).then((res) => res.json())
+}
+
+export function addNote(username, note) {
+  username = username.toLowerCase().trim();
+  const url = `https://jbgithub-notetaker.firebaseio.com/${username}.json`;
+  return fetch(url, {
+   method: 'post',
+   body: JSON.stringify(note)
+  }).then((res) => res.json());
+}
